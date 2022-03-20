9800bab7816b92417e3035748cb258dfe90c7537
Steve Lee <steve.patriot@gmail.com>
2022-03-20 14:17:16 -0400

    여기서 배운것 중에서 가장 중요한 것부터 얘기하자면..
    1. private var listener : View.OnClickListener? = null
       listener = View.OnClickListener { it:View!
       when (it.id) {
           R.id.button1 -> sendOnChannel1(x,y,z)
       } //이정도면 확실히 이해하겠지? 더 설명해야돼?
       button1.setOnClickListener(listener)
    
    2. private var mManager : NotificationManager? = null
       if (mManager == null) {
          mManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
          //여기에서보듯이 캐스팅을 첫번째로 해준다.
       }
       return mManager as NotificationManager
       //여기서 보듯이 리턴할 때에도 또 캐스팅을 해준다. 왜?????
    
    3. NotificationChannel을 사용해야 하는 걸 배웠고..
       NotificationBuilder를 통해서 실제로 화면에 그리고 라이트도 넣는걸 배웠고..
       NotificationManager를 통해서 위의 두개를 연결해서 실제로 보여주는 기능을 한다. 이때 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager을 해준다는걸 기억하자.. 그리고 여기에서 오류가 나왔고.. 해결책은
       return mManager as NotificationManager를 또다시 캐스팅을 해주었다는 거다.
       수고했다..
    4. 이제 여기에서 Notification을 눌렀을 때 해당 액티비티를 다시 여는걸 배웠다. Intent를 통해서 여는걸 배웠고 분명히 MainActivity::Class.java라고 하는 부분에 다른 Activity나 Fragment를 넣으면 그쪽으로 가도록 되겠지..
       그리고 여기서도 코틀린을 사용하는데도 java가 여전히 같이 사용되고 있는걸 기억하고
       또 한가지...Array<Intent>!이렇게 되어있다는건 Array를 받아라는 거고.. Intent가 멤버로 들어가 있는거고 !는 null이 되면 안된다는거고.. 그래서 ArrayOf(intent)이렇게 ArrayOf를 사용하였다.
    5. 마지막으로 manifest.xml파일에서 최상위로만 사용되도록 singleTop을 적용해주어야한다.
    
    이번 프로젝트는 아주 많은 곳에 응용이 될것이며 훌륭한 예제였고 알아야 할 내용이 참 많은 좋은 예제였다.
    
    --------------------------------------------
    여기에서 내가 오늘 공부하며 적용했던 내용들을 한번 정리하고 넘어간다.
    OOP 프로그래밍 개발시 가장 중요하게 생각해야 하는 것
    * 어떤 메커니즘을 사용할 것인가?
    * 어떤식으로 디자인을 할 것인가?
    * 어떤 데이터를 사용할 것인가? 여기서 데이터는 함수, 변수, 클래스, 람다, 타임 등이 있겠다.
    * 어떤 데이터를 외부로부터 가지고 올것인가? - 이부분이 아주 헷갈렸지만 잘 생각해보면 답을 얻을 수 있다.
    * 어떤 함수나 변수나 클래스를 외부에 노출할 것인가? - 디자인측면에서 get과  set을 잘 생각해보도록 하고 외부에 노출되는 변수들과 함수들도 잘 기억하도록 하자.. 그 외부 노출되는것들이 결국 외부와의 자료를 주고 받도록 하는 부분이니깐...

--- app/src/main/java/com/example/notificationchannels/MainActivity.kt
+++ app/src/main/java/com/example/notificationchannels/MainActivity.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.core.app.NotificationCompat
-import java.security.spec.MGF1ParameterSpec
 
 class MainActivity : AppCompatActivity() {
     private lateinit var editTextTitle : EditText
@@ -15,47 +14,58 @@ class MainActivity : AppCompatActivity() {
     private lateinit var buttonChannel1 : Button
     private lateinit var buttonChannel2 : Button
     private lateinit var mNotificationHelper: NotificationHelper
-    private var listener : View.OnClickListener? = null
+    private lateinit var listener : View.OnClickListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        memberVariableInit()
+        //이 이후에 나와야지 View를 사용할 수 있는거지..
+        initMemberVariables()
 
     }
 
-    private fun memberVariableInit() {
+    private fun initMemberVariables() {
         editTextMessage = findViewById<EditText>(R.id.edittext_message)
         editTextTitle = findViewById<EditText>(R.id.edittext_title)
         buttonChannel1 = findViewById<Button>(R.id.button_channel1)
         buttonChannel2 = findViewById<Button>(R.id.button_channel2)
         buttonsOnClickListener()
-        listener = View.OnClickListener {
-            when (it.id) {
-                R.id.button_channel1 -> sendOnChannel1(editTextTitle.text.toString(), editTextMessage.text.toString())
-                R.id.button_channel2 -> sendOnChannel2(editTextTitle.text.toString(), editTextMessage.text.toString())
-            }
-        }
+        //순서는 바뀌면 안된다는 것..
         buttonChannel1.setOnClickListener(listener)
         buttonChannel2.setOnClickListener(listener)
         mNotificationHelper = NotificationHelper(applicationContext)
 
     }
     private fun buttonsOnClickListener() {
+        listener = View.OnClickListener {
+            when (it.id) {
+                R.id.button_channel1 -> sendOnChannel1(editTextTitle.text.toString(), editTextMessage.text.toString())
+                R.id.button_channel2 -> sendOnChannel2(editTextTitle.text.toString(), editTextMessage.text.toString())
+            }
+        }
     }
 
     private fun sendOnChannel1(title : String, message : String) {
         var nb : NotificationCompat.Builder = mNotificationHelper.getChannel1Notification(title,message)
         //여기 외부에서 연결시켜주는구나.. 비로서 헬퍼클래스에서 만든 모든 것들을 연결시키기 시작하는데..
         //일단 NotificationCompat.Builder 클래스를 만든 인스턴스 변수를 가지고있고..
-        //getManager를 통해서 NotificationManager를 받아서 그 메이저에게 Notifiy하는데 id : 1이고 builder클래스를
-        //build 하라는 거구나 결국 메니저가 하는거지.. 뭐에 의해서 notify에 의해서
+        //mNotificationHelper클래서에 있는 getChannel1Notification을 실행할 때
+        //이 안에서 어떤일이 일어나는지는 모르지만 분명히 CHANNELID1을 통해서 mManager에 대한
+        //Manager가 정해질거고 그리고 그 CHANNELID1에 해당하는 NotificaionChannel을
+        //안드로이드 노티피케이션 메니저가 관리 할 수 있도록 한다.
+        //notify의 id는 화면뿌려주는 기능을 할때 똑같은 아이디가 있으면 리플레이스를 해준다.
         mNotificationHelper.getManager().notify(1, nb.build())
-
+        //위의 내용이 결국 화면에 뿌려주는 기능인데 이름을 잘봐라. send on channel1이잖아. ㅋ
     }
 
     private fun sendOnChannel2(title : String, message : String) {
         var nb : Notification.Builder = mNotificationHelper.getChannel2Notification(title,message)
         mNotificationHelper.getManager().notify(2, nb.build())
+
+        //그러면 클릭은 어디서 연결할 수 있을까? 상식적으로 클릭은 Vew에서 setOnClickListener를 하는건데..
+        //화면에 뿌려주는 부분의 Context는 이제 NotificationManager에서 하지 않을까?
+        //그러면 클릭리스너도 Manager에서 관리해줘야하지 않을까?
+        //어디서 해야할까? 모르겠다.
     }
 
 }
--- app/src/main/java/com/example/notificationchannels/NotificationHelper.kt
+++ app/src/main/java/com/example/notificationchannels/NotificationHelper.kt
@@ -4,17 +4,24 @@ import android.annotation.TargetApi
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.ContextWrapper
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 
 //Helper Class가 왜 필요한걸까???
+//화면에 데이터를 뿌려주는 기능들도 같이 들어가야 하니깐 데이터와 화면의 내용을 위해서 helper클래스가 필요하다.
 class NotificationHelper(base: Context?) : ContextWrapper(base) {
     //지금 계속 null값과 관련된 캐스팅에 문제가 계속 있네.. Kotlin이 이걸 강력하게 제한하고 있어서.. 그렇구나..
     //해결하자.. 이거 해결해야지 잘수있다.
+    //여기 프로그래밍에서 보듯이 mManager은 초기에 만들때 null인지 확인을 하고 null이면
+    //새로 생성하고 또 1, 2를 이렇게 생성한다.
+    //그러니깐 laitinit 을 한다고 하면서 그냥 넘어가면 말그대로 null값이 들어올 수 있으니 오류가 생기는거지..
+
     private var mManager : NotificationManager? = null
 
     companion object {
@@ -49,7 +56,7 @@ class NotificationHelper(base: Context?) : ContextWrapper(base) {
         channel2.enableVibration(true)
         channel2.lightColor = Color.MAGENTA
         channel2.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
-        getManager().createNotificationChannel(channel2)
+        mManager?.createNotificationChannel(channel2)
     }
 
     public fun getManager() : NotificationManager {
@@ -58,16 +65,31 @@ class NotificationHelper(base: Context?) : ContextWrapper(base) {
                 //이부분 정말 중요하다.
             mManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         }
+        //여기서 위에와 밑에 두번 캐스팅을 해주었다.
         return mManager as NotificationManager
     }
     public fun getChannel1Notification(title : String, message : String) : NotificationCompat.Builder {
+        //여기에서 만드는구나... 그래 그렇게도 생각했었지...
+        //여기서도 MainActivity::class.java라고 쓰는구나. java를 사용하는구나.
+        val resultIntent : Intent = Intent(this, MainActivity::class.java)
+        //위에 내 intent를 만들었고.. Pending Intent를 만드는 이유는
+        /* If you give the foreign application an Intent, it will execute your Intent with its own permissions
+        But if you give the foreign application a PendingIntent, that application will execute
+        your Intent using your application's permission
+         */
+        val resultPendingIntent = PendingIntent.getActivities(this, 1,
+            arrayOf(resultIntent), PendingIntent.FLAG_UPDATE_CURRENT)
         return NotificationCompat.Builder(applicationContext, CHANNEL1ID)
             .setContentTitle(title)
             .setContentText(message)
             .setSmallIcon(R.drawable.ic_one)
+            .setAutoCancel(true)
+            .setOngoing(false)//setOngoing이 Notification을 지워주는 플래그이구나.
+            .setContentIntent(resultPendingIntent)
     }
     public fun getChannel2Notification(title : String, message : String) : Notification.Builder {
         return Notification.Builder(applicationContext, CHANNEL2ID)
+            .setColor(Color.RED)
             .setContentTitle(title)
             .setContentText(message)
             .setSmallIcon(R.drawable.ic_two)
